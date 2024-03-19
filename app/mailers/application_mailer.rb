727b576416594ec92f82c96a177eb2542bb176bf
Rubens Gomes Neto <rubens.gomes.neto@protonmail.com>
2024-03-19 06:34:49 -0300

    Adiciona gema rubocop
    Força normas de estilo de código no projeto.
    
    Signed-off-by: Rubens Gomes Neto <rubens.gomes.neto@protonmail.com>

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
+# Mailer padrão da aplicação, gerado junto pelo comando de criação da aplicação
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  default from: 'from@example.com'
+  layout 'mailer'
 end
