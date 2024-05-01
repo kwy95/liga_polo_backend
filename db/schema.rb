8d294a634c3bbf3ec80b414d9df2cb451daf56ef
Rubens Gomes Neto <rubens.gomes.neto@protonmail.com>
2024-05-01 17:06:07 -0300

    Criar modelo Pessoa
    Esse modelo vai representar as pessoas que participarem de liga.
    
    Signed-off-by: Rubens Gomes Neto <rubens.gomes.neto@protonmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_01_192339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pessoas", force: :cascade do |t|
+    t.string "nome", null: false
+    t.date "data_nascimento"
+    t.boolean "eh_mtnb", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["nome"], name: "index_pessoas_on_nome"
+  end
 
 end
