22f4f748275f8011405bdf2f251fedc143a11222
Rubens Gomes Neto <rubens.gomes.neto@protonmail.com>
2024-05-01 22:50:43 -0300

    Criar modelo Partida
    Uma partida de um evento.
    
    Signed-off-by: Rubens Gomes Neto <rubens.gomes.neto@protonmail.com>

---
+++ app/models/partida.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: partidas
+#
+#  id         :bigint           not null, primary key
+#  ordem      :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  fase_id    :bigint           not null
+#
+# Indexes
+#
+#  index_partidas_on_fase_id            (fase_id)
+#  index_partidas_on_fase_id_and_ordem  (fase_id,ordem) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (fase_id => fases.id)
+#
+class Partida < ApplicationRecord
+  belongs_to :fase
+
+  validates :ordem, uniqueness: { scope: :fase_id }
+end
---
+++ spec/models/partida_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: partidas
+#
+#  id         :bigint           not null, primary key
+#  ordem      :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  fase_id    :bigint           not null
+#
+# Indexes
+#
+#  index_partidas_on_fase_id            (fase_id)
+#  index_partidas_on_fase_id_and_ordem  (fase_id,ordem) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (fase_id => fases.id)
+#
+require 'rails_helper'
+
+RSpec.describe Partida do
+  let(:partida) { build(:partida) }
+
+  it 'esperado que seja válido' do
+    expect(partida).to be_valid
+  end
+
+  describe 'Associações' do
+    it { is_expected.to belong_to(:fase) }
+  end
+
+  describe 'Validações' do
+    it { expect(partida).to validate_uniqueness_of(:ordem).scoped_to(:fase_id) }
+  end
+end
